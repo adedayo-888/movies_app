c89975c38ef05caf2b24a5d2707aecf676ceed63
adedayo-888 <dayoadeniji69@yahoo.com>
2022-01-07 13:48:33 +0000

    Header Changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,59 +1,45 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import { AppBar, Typography, Toolbar, Box } from '@mui/material';
 
-import { Typography, Toolbar, Box } from '@mui/material';
-import MuiAppBar from '@mui/material/AppBar';
-
-const drawerWidth = 280;
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginRight: drawerWidth,
-  }),
-}));
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: 'flex-start',
-}));
-
-export default function PersistentDrawerRight() {
+const Header = () => {
   return (
     <>
-      <Box sx={{ display: 'flex' }}>
-        <AppBar position='fixed'>
-          <Toolbar sx={{ background: 'red', alignItems: 'center' }}>
+      <AppBar position='fixed'>
+        <Toolbar
+          sx={{
+            background: 'red',
+            display: 'flex',
+            alignItems: 'center',
+            fontWeight: 'bold',
+            justifyContent: 'space-between',
+            height: '90px',
+            padding: '0 20px',
+          }}>
+          <Box>
             <Typography
-              variant='h6'
+              variant='h4'
               gutterBottom
               noWrap
               sx={{ cursor: 'pointer' }}
               onClick={() => window.scroll(0, 0)}
               component='div'>
-              <strong>CloudFlix</strong>
-              <br />
-              <Typography gutterBottom variant='body2'>
-                <b>Your Movie Search App</b>
-              </Typography>
+              CloudFlix
             </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
+          </Box>
+          <Box>
+            <Typography
+              variant='h6'
+              sx={{ cursor: 'pointer' }}
+              onClick={() => window.scroll(0, 0)}
+              component='div'>
+              <b>Your Movie/TV Search App</b>
+            </Typography>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Toolbar></Toolbar>
     </>
   );
-}
+};
+
+export default Header;
